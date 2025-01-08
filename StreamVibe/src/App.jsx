9288025e0a551d023a91ce2668ff4627db5abe70
Mustapha Osman <wnfynn@gmail.com>
2025-01-08 19:30:54 +0000

    using the tmbd api

--- StreamVibe/src/App.jsx
+++ StreamVibe/src/App.jsx
@@ -4,8 +4,29 @@ import MoviesXShows from "./pages/MoviesXShows";
 import Support from "./pages/Support";
 import Subscriptions from "./pages/Subscriptions";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const url = "https://jsonplaceholder.typicode.com/comments/";
+
+  let [data, setData] = useState();
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        const names = data.map((value) => value.id);
+        setData(names);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
+
   return (
     <div className="text-white">
       <Navbar />

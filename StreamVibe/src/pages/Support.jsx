69ba041186297fbffc5b4ab63e4564522aeea8fb
Mustapha Osman <wnfynn@gmail.com>
2025-01-14 17:53:19 +0000

    building support page

--- StreamVibe/src/pages/Support.jsx
+++ StreamVibe/src/pages/Support.jsx
@@ -1,8 +1,23 @@
-
 const Support = () => {
   return (
-    <div>Support</div>
-  )
-}
+    <div>
+      <div className="border-2 h-[15vh]"></div>
+      <div>
+        <div>
+          <div>
+            <h1 className="5s:text-[2rem] sm:text-[2.5rem] max-5s:text-[1rem] font-extrabold">
+              Welcome to our support page!
+            </h1>
+            <p className="mt-5 max-5s:text-[0.8rem] text-[#999999]">
+              We&apos;re here to help you with any problems you may be having
+              with our product.
+            </p>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
-export default Support
+export default Support;

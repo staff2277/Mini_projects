9846d0d38f0c5ca9ba76ce5fca25804456c6bf1a
Mustapha Osman <wnfynn@gmail.com>
2025-01-16 17:29:40 +0000

    done with table

---
+++ StreamVibe/components/SubTable.jsx
@@ -0,0 +1,92 @@
+const SubTable = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <h2 className="5s:text-[2rem] sm:text-[2.5rem] max-5s:text-[1rem] font-extrabold">
+              Compare our plans and find the right one for you
+            </h2>
+          </div>
+          <p className="my-3 text-[#999999] sm:text-[1rem] max-5s:text-[0.8rem]">
+            StreamVibe offers three different plans to fit your needs: Basic,
+            Standard, and Premium. Compare the features of each plan and choose
+            the one that&apos;s right for you.
+          </p>
+        </div>
+
+        <div className="h-full flex mt-[4rem]">
+          <div className="*:border-[1px] *:px-5 *:border-[#1F1F1F] grid grid-rows-[repeat(10,90px)] *:flex *:items-center text-[#999999] sm:text-[1rem] max-5s:text-[0.8rem]">
+            <div className="bg-[#0F0F0F] text-white font-extrabold">
+              Features
+            </div>
+            <div>Price</div>
+            <div>Content</div>
+            <div>Devices</div>
+            <div>Free Trail</div>
+            <div>Cancel Anytime</div>
+            <div>HDR</div>
+            <div>Dolby Atmos</div>
+            <div>Ad - Free</div>
+            <div>Offline Viewing</div>
+            <div>Family Sharing</div>
+          </div>
+          <div className="*:border-[1px] *:px-5 *:border-[#1F1F1F] grid grid-rows-[repeat(10,90px)] *:flex *:items-center text-[#999999] sm:text-[1rem] max-5s:text-[0.8rem]">
+            <div className="bg-[#0F0F0F] text-white font-extrabold">Basic</div>
+            <div>$9.99/Month</div>
+            <div>
+              Access to a wide selection of movies and shows, including some new
+              releases.
+            </div>
+            <div>Watch on one device simultaneously</div>
+            <div>7 Days</div>
+            <div>Yes</div>
+            <div>No</div>
+            <div>No</div>
+            <div>No</div>
+            <div>No</div>
+            <div>No</div>
+          </div>
+          <div className="*:border-[1px] *:px-5 *:border-[#1F1F1F] grid grid-rows-[repeat(10,90px)] *:flex *:items-center text-[#999999] sm:text-[1rem] max-5s:text-[0.8rem]">
+            <div className="bg-[#0F0F0F] text-white font-extrabold">
+              Standard
+            </div>
+            <div>$12.99/Month</div>
+            <div>
+              Access to a wider selection of movies and shows, including most
+              new releases and exclusive content
+            </div>
+            <div>Watch on Two device simultaneously</div>
+            <div>7 Days</div>
+            <div>Yes</div>
+            <div>Yes</div>
+            <div>Yes</div>
+            <div>Yes</div>
+            <div>Yes, for select titles.</div>
+            <div>Yes, up to 5 family members.</div>
+          </div>
+          <div className="*:border-[1px] *:px-5 *:border-[#1F1F1F] grid grid-rows-[repeat(10,90px)] *:flex *:items-center text-[#999999] sm:text-[1rem] max-5s:text-[0.8rem]">
+            <div className="bg-[#0F0F0F] text-white font-extrabold">
+              Premium
+            </div>
+            <div>$14.99/Month</div>
+            <div>
+              Access to a widest selection of movies and shows, including all
+              new releases and Offline Viewing
+            </div>
+            <div>Watch on Four device simultaneously</div>
+            <div>7 Days</div>
+            <div>Yes</div>
+            <div>Yes</div>
+            <div>Yes</div>
+            <div>Yes</div>
+            <div>Yes, for all titles.</div>
+            <div>Yes, up to 6 family members.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubTable;
--- StreamVibe/src/pages/Subscriptions.jsx
+++ StreamVibe/src/pages/Subscriptions.jsx
@@ -1,8 +1,26 @@
+import SubPlan from "../../components/SubPlan";
+import SubTable from "../../components/SubTable";
+import Trial from "../../components/Trial";
+import Footer from "../../components/Footer";
 
 const Subscriptions = () => {
   return (
-    <div>Subscriptions</div>
-  )
-}
+    <div className="">
+      <div className="h-[15vh] border-2"></div>
+      <div className="sm:px-[5%] max-sm:px-[15px]">
+        <SubPlan />
+      </div>
+      <div className="mt-5rem sm:px-[5%] max-sm:px-[15px]">
+        <SubTable />
+      </div>
+      <div className="mt-[7rem] sm:px-[5%] max-sm:px-[15px]">
+        <Trial />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default Subscriptions
+export default Subscriptions;
